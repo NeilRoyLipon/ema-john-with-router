4575af5a0d0bc6681e2a47ad77205b40cf11816a
Lipon <neilgates13@gmail.com>
2021-10-01 07:37:23 +0600

    Second commit

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is Inventory Page</h2>
+            <h2>This is Inventory Page created by Lipon</h2>
         </div>
     );
 };
